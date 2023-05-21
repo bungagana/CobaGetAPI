4d412d5f5139ae3c97720a6f7cdc0db5cd84286c
bungagana <bungamagelang57@gmail.com>
2023-05-21 14:12:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cobaget/ApiService.kt
@@ -0,0 +1,10 @@
+package com.example.cobaget
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("datamahasiswa/")
+    fun getdatamahasiswa(): Call<ApiResponse>
+}
+
